1ce9b67a09f7df31728a9706f25dbfcfb47a8b76
nilangav <nilangavirajith.13@cse.mrt.ac.lk>
2023-06-11 13:28:01 +0800

    Updated recommendations and removed unnecessary divs

--- js/main.js
+++ js/main.js
@@ -293,6 +293,23 @@ $(function() {
 
   bar.animate(.70);
 
+  var bar = new ProgressBar.Line(lineprog9, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    delay: 3200,
+    trailWidth: 1.72,
+    svgStyle: {
+      width: '100%',
+      height: '100%'
+    },
+    step: (state, bar) => {
+      bar.setText(Math.round(bar.value() * 100) + ' %');
+    }
+  });
+
+  bar.animate(.75);
+
   // Contact form
   $('.art-input').keyup(function() {
     if ($(this).val()) {
